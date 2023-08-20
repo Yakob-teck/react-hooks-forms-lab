a0113510f82df7c3a5dc36ea407e2631c76947f2
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-08-20 08:46:56 -0600

    filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, onSearchChange }) {
+  const [searchText, setSearchText] = useState("");
+
+  function handleSearchChange(event) {
+    const newSearchText = event.target.value;
+    setSearchText(newSearchText);
+    onSearchChange(newSearchText); // Notify parent component of search change
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        value={searchText}
+        onChange={handleSearchChange}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
